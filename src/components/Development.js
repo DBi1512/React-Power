ff4645f357e33fec47602850bf1107f4f9cf60be
Dean Phan <dean.t.phan@gmail.com>
2019-04-25 13:52:38 +0300

    checking input checked

--- src/components/Development.js
+++ src/components/Development.js
@@ -1,39 +1,44 @@
 import React from "react";
 import Data from "./Data";
 
-// class Development extends React.Component {
-//   render() {
-//     const { label, title, para, tag, btn } = this.props;
-//     return (
-//       <div className="tab">
-//         <label htmlFor={label}>{label}</label>
-//         <input id={label} name="checkbox-tab-group" type="radio" className="checkboxtab" />
+class Development extends React.Component {
+  render() {
+    const { label, title, para, tag, btn } = this.props;
+    return (
+      <div className="tab">
+        <label htmlFor={label}>{label}</label>
+        <input id={label} value={label} name="checkbox-tab-group" type="radio" className="checkboxtab" />
 
-//         <div className="content">
-//           <h5>{title}</h5>
-//           <p>{para} </p>
-//           <a href={tag}>{btn}</a>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+        <div className="content">
+          <h5>{title}</h5>
+          <p>{para} </p>
+          <a href={tag}>{btn}</a>
+        </div>
+      </div>
+    );
+  }
+}
 
 // label same with id, click the label to specify which input is used
-const Development = ({ label, title, para, tag, btn }) => {
-  return (
-    <div className="tab">
-      <label htmlFor={label}>{label}</label>
-      <input id={label} name="checkbox-tab-group" type="radio" className="checkboxtab" />
+// const Development = ({ label, title, para, tag, btn }) => {
+//   return (
+//     <div className="tab">
+//       <label htmlFor={label}>{label}</label>
+//       <input
+//         id={label}
+//         value={label}
+//         name="checkbox-tab-group"
+//         type="radio"
+//         className="checkboxtab" />
 
-      <div className="content">
-        <h5>{title}</h5>
-        <p>{para} </p>
-        <a href={tag}>{btn}</a>
-      </div>
-    </div>
-  );
-};
+//       <div className="content">
+//         <h5>{title}</h5>
+//         <p>{para} </p>
+//         <a href={tag}>{btn}</a>
+//       </div>
+//     </div>
+//   );
+// };
 
 const renderDevelopment = Data.development.map(({ label, title, para, tag, btn }, index) => (
   <Development key={index} label={label} title={title} para={para} tag={tag} btn={btn} />
