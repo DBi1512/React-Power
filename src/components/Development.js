6c05a31568636d2e0f96abb2c1f9d9d6b2bd2e9a
StevePhuc <stevephuc.dev@gmail.com>
2019-04-25 10:31:58 +0300

    tab change

--- src/components/Development.js
+++ src/components/Development.js
@@ -1,30 +1,34 @@
 import React from "react";
 import Data from "./Data";
 
-const Development = ({ label, title, para, tag, btn }) => {
-  return (
-    <div className="tab">
-      <input
-        name="checkbox-tab-group"
-        type="radio"
-        id="checkbox1"
-        className="checkboxtab"
-        checked
-        onChange={function() {}}
-      />
-      <label htmlFor="checkbox1">{label}</label>
-
-      <div className="content">
-        <h5>{title}</h5>
-        <p>{para} </p>
-        <a href={tag}>{btn}</a>
+// const Development = ({ label, title, para, tag, btn, checked }) => {
+class Development extends React.Component { 
+  render(){
+    const { label, title, para, tag, btn }= this.props
+    // https://react.tips/radio-buttons-in-react-16/
+    return (
+      <div className="tab">
+        <input
+          name="checkbox-tab-group"
+          type="radio"
+          id={label} 
+          value={label}
+          className="checkboxtab"
+        />
+        <label htmlFor={label}>{label}</label>
+  
+        <div className="content">
+          <h5>{title}</h5>
+          <p>{para} </p>
+          <a href={tag}>{btn}</a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 const renderDevelopment = Data.development.map(({ label, title, para, tag, btn }, index) => (
-  <Development key={index} label={label} title={title} para={para} tag={tag} btn={btn} />
+  <Development key={index} label={label} title={title} para={para} tag={tag} btn={btn}  />
 ));
 
 export default renderDevelopment;
