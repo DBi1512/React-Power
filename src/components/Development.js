07b02b8b730e65ac6909c074b902f457d843d7cb
Dean Phan <dean.t.phan@gmail.com>
2019-04-25 14:27:37 +0300

    style fixed

--- src/components/Development.js
+++ src/components/Development.js
@@ -1,47 +1,43 @@
 import React from "react";
 import Data from "./Data";
 
-class Development extends React.Component {
-  render() {
-    const { label, title, para, tag, btn } = this.props;
-    return (
-      <div className="tab">
-        <label htmlFor={label}>{label}</label>
-        <input id={label} value={label} name="checkbox-tab-group" type="radio" className="checkboxtab" />
-
-        <div className="content">
-          <h5>{title}</h5>
-          <p>{para} </p>
-          <a href={tag}>{btn}</a>
-        </div>
-      </div>
-    );
-  }
-}
-
 // label same with id, click the label to specify which input is used
-// const Development = ({ label, title, para, tag, btn }) => {
-//   return (
-//     <div className="tab">
-//       <label htmlFor={label}>{label}</label>
-//       <input
-//         id={label}
-//         value={label}
-//         name="checkbox-tab-group"
-//         type="radio"
-//         className="checkboxtab" />
+// NOTE: Label HAS TO be under input for Checked to work
+const Development = ({ label, title, para, tag, btn }) => {
+  return (
+    <div className="tab">
+      <input id={label} value={label} name="checkbox-tab-group" type="radio" className="checkboxtab" />
+      <label htmlFor={label}>{label}</label>
 
-//       <div className="content">
-//         <h5>{title}</h5>
-//         <p>{para} </p>
-//         <a href={tag}>{btn}</a>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="content">
+        <h5>{title}</h5>
+        <p>{para} </p>
+        <a href={tag}>{btn}</a>
+      </div>
+    </div>
+  );
+};
 
 const renderDevelopment = Data.development.map(({ label, title, para, tag, btn }, index) => (
   <Development key={index} label={label} title={title} para={para} tag={tag} btn={btn} />
 ));
 
 export default renderDevelopment;
+
+// class Development extends React.Component {
+//   render() {
+//     const { label, title, para, tag, btn } = this.props;
+//     return (
+//       <div className="tab">
+//         <input id={label} value={label} name="checkbox-tab-group" type="radio" className="checkboxtab" />
+//         <label htmlFor={label}>{label}</label>
+
+//         <div className="content">
+//           <h5>{title}</h5>
+//           <p>{para} </p>
+//           <a href={tag}>{btn}</a>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
